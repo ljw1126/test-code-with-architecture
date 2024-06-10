e8e3d41388723c6c2627167026fe6fff5ba62681
leejinwoo <leejinwoo1126@gmail.com>
2024-06-10 20:27:22 +0900

    test: h2 이용한 repository 테스트 (2-2)

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus로_유저데이터를_찾아올수있다() {
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    void findByIdAndStatus는_데이터가없으면_Optional_empty를반환한다() {
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(2, UserStatus.PENDING);
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void findByEmailAndStatus로_유저데이터를_찾아올수있다() {
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.PENDING);
+
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    void findByEmailAndStatus는_데이터가없으면_Optional_empty를반환한다() {
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.ACTIVE);
+
+        assertThat(result).isEmpty();
+    }
+}
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values('1', 'leejinwoo1126@gmail.com', 'ljw1126', 'Busan', 'test-code', 'PENDING', 0);
