bf7d03ed4d0a0b64d41eff1501345d4bd387c846
leejinwoo <leejinwoo1126@gmail.com>
2024-06-10 20:50:29 +0900

    test: 테스트 데이터 sql 추가

--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,4 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values('1', 'leejinwoo1126@gmail.com', 'ljw1126', 'Busan', 'test-code', 'PENDING', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values('2', 'wkrdmsdmffn@naver.com', 'wkrdms', 'Busan', 'test-code2', 'ACTIVE', 0);
