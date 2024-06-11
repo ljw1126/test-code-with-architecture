9ed7b635fcb31a08187ba343baa5d8ba67abbcf9
leejinwoo <leejinwoo1126@gmail.com>
2024-06-11 19:48:09 +0900

    refactor: repository 의존성 역전 적용

--- src/test/java/com/example/demo/user/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/user/controller/UserControllerTest.java
@@ -3,7 +3,7 @@ package com.example.demo.user.controller;
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.domain.UserUpdate;
 import com.example.demo.user.infrastructure.UserEntity;
-import com.example.demo.user.infrastructure.UserRepository;
+import com.example.demo.user.infrastructure.UserJpaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +32,7 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserJpaRepository userJpaRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -58,7 +58,7 @@ class UserControllerTest {
         mockMvc.perform(get("/api/users/1/verify").queryParam("certificationCode", "test-code"))
                 .andExpect(status().isFound());
 
-        UserEntity userEntity = userRepository.findById(2L).get();
+        UserEntity userEntity = userJpaRepository.findById(2L).get();
         assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
     }
 
--- src/test/java/com/example/demo/user/infrastructrue/UserRepositoryTest.java
+++ src/test/java/com/example/demo/user/infrastructrue/UserRepositoryTest.java
@@ -2,7 +2,7 @@ package com.example.demo.user.infrastructrue;
 
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.infrastructure.UserEntity;
-import com.example.demo.user.infrastructure.UserRepository;
+import com.example.demo.user.infrastructure.UserJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,32 +23,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserRepositoryTest {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserJpaRepository userJpaRepository;
 
     @Test
     void findByIdAndStatus로_유저데이터를_찾아올수있다() {
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1L, UserStatus.PENDING);
 
         assertThat(result).isPresent();
     }
 
     @Test
     void findByIdAndStatus는_데이터가없으면_Optional_empty를반환한다() {
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(2, UserStatus.PENDING);
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(2, UserStatus.PENDING);
 
         assertThat(result).isEmpty();
     }
 
     @Test
     void findByEmailAndStatus로_유저데이터를_찾아올수있다() {
-        Optional<UserEntity> result = userRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.PENDING);
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.PENDING);
 
         assertThat(result).isPresent();
     }
 
     @Test
     void findByEmailAndStatus는_데이터가없으면_Optional_empty를반환한다() {
-        Optional<UserEntity> result = userRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.ACTIVE);
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("leejinwoo1126@gmail.com", UserStatus.ACTIVE);
 
         assertThat(result).isEmpty();
     }
