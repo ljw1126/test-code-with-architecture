b25c82ddbb1bd46f3d42769bee08a33ca2d804be
leejinwoo <leejinwoo1126@gmail.com>
2024-06-11 14:51:26 +0900

    refactor: 테스트 패키지 구조 변경

--- src/test/java/com/example/demo/user/infrastructrue/UserRepositoryTest.java
+++ src/test/java/com/example/demo/user/infrastructrue/UserRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.repository;
+package com.example.demo.user.infrastructrue;
 
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.infrastructure.UserEntity;
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
 import java.util.Optional;
 
@@ -15,7 +16,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest(showSql = true)
 @TestPropertySource("classpath:test-application.properties")
-@Sql("/sql/user-repository-test-data.sql")
+@SqlGroup({
+        @Sql(value = "/sql/user-repository-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class UserRepositoryTest {
 
     @Autowired
